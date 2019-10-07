75eebaec3476db3bfbbc3e2901f6f857333978e2
Jomon Johnson <jomon.entero@gmail.com>
2019-10-07 15:58:50 +0530

    clipboard js

--- config/params.php
+++ config/params.php
@@ -4,4 +4,9 @@ return [
     'app.storage.url' => @$_ENV['STORAGE_URL'],
 
     'fileStorage.filesystemComponent' => 'fsLocal',
+
+    'fsAWS.key' => '',
+    'fsAWS.secret' => '',
+    'fsAWS.bucket' => '',
+    'fsAWS.region' => '',
 ];
